5c6fb117cbdb7ff48b679cdaa3b204b8f35c6e9a
Abdulrahman Mohamed Abougendia <aabougen@eceprog3.ecn.purdue.edu>
2025-09-20 18:43:44 -0400

    I/O works, fixes #34

--- src/cli_project/cli.py
+++ src/cli_project/cli.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import sys
 import subprocess
+import json, sys, time
 
 def install() -> None:
     """Implements ./run install"""
@@ -28,9 +29,61 @@ def score(url_file: str) -> None:
         sys.stderr.write(f"File not found: {path}\n")
         sys.exit(1)
 
-    for line in path.read_text(encoding="utf-8").splitlines():
-        if line.strip():
-            print(line)
+    with path.open("r", encoding="utf-8") as f:
+        urls = [line.strip() for line in f if line.strip()]
+    
+    dataset_urls = []
+    code_urls = []
+
+    for url in urls:
+        if "huggingface.co/datasets" in url:
+            dataset_urls.append(url)
+        elif "github.com" in url:
+            code_urls.append(url)
+        elif "huggingface.co" in url and "/tree/" in url:  # MODEL
+            record = {
+                "name": url,
+                "category": "MODEL",
+                "net_score": 0.0,
+                "net_score_latency": 0,
+                "ramp_up_time": 0.0,
+                "ramp_up_time_latency": 0,
+                "bus_factor": 0.0,
+                "bus_factor_latency": 0,
+                "performance_claims": 0.0,
+                "performance_claims_latency": 0,
+                "license": 0.0,
+                "license_latency": 0,
+                "size_score": {
+                    "raspberry_pi": 0.0,
+                    "jetson_nano": 0.0,
+                    "desktop_pc": 0.0,
+                    "aws_server": 0.0,
+                },
+                "size_score_latency": 0,
+                "dataset_and_code_score": 0.0,
+                "dataset_and_code_score_latency": 0,
+                "dataset_quality": 0.0,
+                "dataset_quality_latency": 0,
+                "code_quality": 0.0,
+                "code_quality_latency": 0,
+            }
+
+            if dataset_urls:
+                record["linked_datasets"] = dataset_urls
+            if code_urls:
+                record["linked_code"] = code_urls
+
+            print(json.dumps(record))
+
+            dataset_urls = []
+            code_urls = []
+
+        else: 
+            sys.stderr.write(f"Warning: {url} not recognized as dataset/code/model URL\n")
+            continue
+
+        
     sys.exit(0)
 
 if __name__ == "__main__":
